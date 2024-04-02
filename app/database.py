3f6739d2871a63e692d63709b7756afb30f8e6bc
achrafjbr <developerjbr@gmail.com>
2024-04-02 05:52:54 +0000

    switch to sqlite instead mysql cuz render could'nt accept mysql

--- app/database.py
+++ app/database.py
@@ -2,9 +2,12 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "mysql+mysqlconnector://root:@127.0.0.1:3306/note_app_db"
 
-engine = create_engine(DATABASE_URL)
+# DATABASE_URL = "mysql+mysqlconnector://root:@127.0.0.1:3306/note_app_db"
+
+DATABASE_URL = 'sqlite:///./sql_app.note_app_db.db'
+
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
