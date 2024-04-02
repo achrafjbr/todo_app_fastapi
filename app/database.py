eab0b4768fb258b5a2ac4917d252d5589dd696db
achrafjbr <developerjbr@gmail.com>
2024-04-02 05:10:51 +0000

    update from localhost to 127.0.0.1 just for test that in deployment on render

--- app/database.py
+++ app/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "mysql+mysqlconnector://root:@localhost:3306/note_app_db"
+DATABASE_URL = "mysql+mysqlconnector://root:@127.0.0.1:3306/note_app_db"
 
 engine = create_engine(DATABASE_URL)
 
